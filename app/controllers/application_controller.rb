92104d54d3bd222a5271cc6700ca9ba786f7a501
Akira Chen <anarchy0316@gmail.com>
2017-11-15 01:29:21 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: "老婆最棒啦！！" 
+  end
+  
+  def goodbye
+    render html: "Mck"
+  end
 end
